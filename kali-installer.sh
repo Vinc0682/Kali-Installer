666904362eecf253c1b2b438c1963ce54dd341e0
VincBreaker <contact@newtownia.net>
2017-02-25 16:44:05 +0100

    Update kali-installer.sh

--- kali-installer.sh
+++ kali-installer.sh
@@ -3,9 +3,8 @@
 echo "This script will install the kali tools on this machine."
 echo "Therefore, it will delete the old sources list!"
 read -r -p "Do you want to continue? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
-    echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]] then
+	echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 	apt-get update
 	apt-get -y install kali-archive-keyring
 	apt-get update
